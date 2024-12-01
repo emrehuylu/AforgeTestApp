c53d827dd82e6ef384184a78d93b986019dd7978
Emre Huylu <emrehuylu10@gmail.com>
2024-12-01 12:58:58 +0300

    Add project files.

---
+++ AforgeNew/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace AforgeNew
+{
+	partial class PictureTake
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(482, 101);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(151, 47);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Başlat";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(34, 31);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(153, 32);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Kameralar:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(211, 41);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(244, 24);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(40, 101);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(418, 262);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(639, 101);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(151, 47);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Yakala";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(482, 165);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(308, 96);
+			this.richTextBox1.TabIndex = 5;
+			this.richTextBox1.Text = "";
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// PictureTake
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(824, 450);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "PictureTake";
+			this.Text = "Fotoğraflama Uygulaması";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ AforgeNew/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+
+
+namespace AforgeNew
+{
+	public partial class PictureTake : Form
+	{
+		public PictureTake()
+		{
+			InitializeComponent();
+		}
+		FilterInfoCollection fico;   //Bilgisayardaki kameraları tutuyor
+		VideoCaptureDevice vcd;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			fico = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+			//Bilgisayarıma bağlı olan kameraların isimlerini combobox'a getirir.
+			foreach (FilterInfo f in fico)
+			{
+				comboBox1.Items.Add(f.Name);  //bağlı tüm kameralar
+				comboBox1.SelectedIndex = 0;  // 0'ıncı indexteki kamera
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			vcd = new VideoCaptureDevice(fico[comboBox1.SelectedIndex].MonikerString);   //Seçilen Kameraya takma bir isim ver.
+			vcd.NewFrame += Vcd_NewFrame;
+			vcd.Start();                //Video Capture Device başlat
+			timer1.Start();
+			richTextBox1.Clear();
+		}
+
+		//kameradan aldığımız çerçevemizi aktarıyoruz.
+		private void Vcd_NewFrame(object sender, NewFrameEventArgs EventArgs)
+		{
+			pictureBox1.Image = (Bitmap)EventArgs.Frame.Clone();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog s = new SaveFileDialog();
+			s.Filter = "(*.jpg) |*.jpg";
+			DialogResult dr = s.ShowDialog();
+			if (dr == DialogResult.OK)
+			{
+				pictureBox1.Image.Save(s.FileName);
+
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			//barkod okuma işlemi
+
+			if (pictureBox1.Image != null)
+			{
+				BarcodeReader brd = new BarcodeReader();
+				Result sonuc = brd.Decode((Bitmap)pictureBox1.Image);
+
+				if (sonuc != null)
+				{
+					richTextBox1.Text = sonuc.ToString();
+					timer1.Stop();
+				}
+			}
+
+		}
+	}
+}
